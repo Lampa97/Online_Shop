47b2521dd49123416f78ea5e7a2c9549ef565007
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-11-26 01:07:38 +0300

    added README and POST response in views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 def contacts(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        return HttpResponse(f"""Спасибо, {name}! 
+        Мы получили ваше сообщение: ({message}).
+        Мы перезвоним вам по телефону: {phone}""")
     return render(request, 'catalog/contacts.html')
 
 def home(request):
