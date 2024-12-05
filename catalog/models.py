a5764d949f12e4ff3271cfd7d0365ef93cefaed2
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-12-05 03:43:02 +0300

    created models Category and Product

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from mypy.main import maybe_write_junit_xml
+
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название', help_text='Введите название категории')
+    description = models.TextField(verbose_name='Описание')
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название', help_text='Введите название продукта')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='catalog/media', blank=True, null=True, verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.FloatField(verbose_name='Цена')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
