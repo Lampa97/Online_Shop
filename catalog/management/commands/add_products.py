a25788a99d8d903feb430731602a9f2e90b6498a
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-12-05 05:10:38 +0300

    updated custom command

--- catalog/management/commands/add_products.py
+++ catalog/management/commands/add_products.py
@@ -9,18 +9,6 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         Category.objects.all().delete()
         Product.objects.all().delete()
-        category, _ = Category.objects.get_or_create(name='Мебель', description='Крепкая древесина')
 
-        products = [
-            {'name': 'Шкаф', 'description': 'Для хранения вашей одежды', 'price': 2000.00, 'category': category},
-             {'name': 'Ящик', 'description': 'Для хранения ваших инструментов', 'price': 899.99, 'category': category},
-              {'name': 'Полка', 'description': 'Если не хватило места для одежды или инструментов', 'price': 599.99, 'category': category}
-        ]
-
-        for product_data in products:
-            product, created = Product.objects.get_or_create(**product_data)
-
-            if created:
-                self.stdout.write(self.style.SUCCESS(f'Successfully added product: {product.name}'))
-            else:
-                self.stdout.write(self.style.WARNING(f'Product already exists: {product.name}'))
+        call_command('loaddata', ['category_fixture.json', 'product_fixture.json'])
+        self.stdout.write(self.style.SUCCESS(f'Successfully loaded data from fixtures'))
