74cc78a1bfca42d2ec5af625a65df6fce401d0da
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-12-05 03:55:43 +0300

    updated model registration

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,12 +5,11 @@ from .models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')
-    list_filter = ('name',)
+    list_display = ('id', 'name')
     search_fields = ('name', 'description')
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'price', 'created_at')
-    list_filter = ('name', 'category', 'price')
-    search_fields = ('name', 'category', 'price', 'description', 'created_at')
+    list_display = ('id', 'name',  'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
