3d290368d60846f388282b9bc7cb9b7dae5915b5
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-12-06 03:07:57 +0300

    used linters

--- catalog/models.py
+++ catalog/models.py
@@ -1,47 +1,48 @@
 from django.db import models
 
-# Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Название', help_text='Введите название категории', unique=True)
-    description = models.TextField(verbose_name='Описание')
+    name = models.CharField(
+        max_length=100, verbose_name="Название", help_text="Введите название категории", unique=True
+    )
+    description = models.TextField(verbose_name="Описание")
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
-        ordering = ['name']
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Название', help_text='Введите название продукта')
-    description = models.TextField(verbose_name='Описание')
-    image = models.ImageField(upload_to='catalog/media', blank=True, null=True, verbose_name='Изображение')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
-    price = models.FloatField(verbose_name='Цена')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    name = models.CharField(max_length=100, verbose_name="Название", help_text="Введите название продукта")
+    description = models.TextField(verbose_name="Описание")
+    image = models.ImageField(upload_to="catalog/media", blank=True, null=True, verbose_name="Изображение")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
+    price = models.FloatField(verbose_name="Цена")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
-        ordering = ['category', 'name']
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["category", "name"]
 
 
 class Contact(models.Model):
-    country = models.CharField(max_length=100, verbose_name='Страна')
-    individual_number = models.CharField(max_length=100, verbose_name='ИНН')
-    address = models.TextField(verbose_name='Адрес')
+    country = models.CharField(max_length=100, verbose_name="Страна")
+    individual_number = models.CharField(max_length=100, verbose_name="ИНН")
+    address = models.TextField(verbose_name="Адрес")
 
     def __str__(self):
         return f"{self.country} {self.address}"
 
     class Meta:
-        verbose_name = 'Контакт'
-        verbose_name_plural = 'Контакты'
-        ordering = ['country', 'address']
+        verbose_name = "Контакт"
+        verbose_name_plural = "Контакты"
+        ordering = ["country", "address"]
--- catalog/views.py
+++ catalog/views.py
@@ -1,26 +1,25 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
-from .models import Product, Contact
+from .models import Contact, Product
 
-# Create your views here.
 
 def contacts(request):
     all_contacts = Contact.objects.all()
 
     if request.method == "POST":
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        return HttpResponse(f"""Спасибо, {name}! 
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+        return HttpResponse(
+            f"""Спасибо, {name}!
         Мы получили ваше сообщение: ({message}).
-        Мы перезвоним вам по телефону: {phone}""")
-    return render(request, 'catalog/contacts.html',
-                  {'contacts': all_contacts}
-                  )
+        Мы перезвоним вам по телефону: {phone}"""
+        )
+    return render(request, "catalog/contacts.html", {"contacts": all_contacts})
 
 
 def home(request):
-    last_5_products = Product.objects.all().order_by('-created_at')[:5]
+    last_5_products = Product.objects.all().order_by("-created_at")[:5]
     print(last_5_products)
-    return render(request, 'catalog/home.html')
+    return render(request, "catalog/home.html")
