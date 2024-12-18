1d546fea14dfbbec79c00a9c4a919d3cc888c890
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-12-18 22:41:06 +0300

    created Article model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Заголовок")
+    content = models.TextField(verbose_name="Содержимое")
+    preview = models.ImageField(upload_to="products", blank=True, null=True, verbose_name="Изображение", default="placeholder.png")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата Создания")
+    is_published = models.BooleanField(default=False, verbose_name="Признак публикации")
+    views_counter = models.PositiveIntegerField(default=0, verbose_name="Количество просмотров")
