f767a0623de7c975c9482cfa69b9f407e9e60e27
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-12-19 09:25:53 +0300

    refactored image fields in Product and Article models

--- catalog/models.py
+++ catalog/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name="Название", help_text="Введите название продукта")
     description = models.TextField(verbose_name="Описание")
     image = models.ImageField(
-        upload_to="products", blank=True, null=True, verbose_name="Изображение", default="placeholder.png"
+        upload_to="products", blank=True, null=True, verbose_name="Изображение",
     )
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
     price = models.FloatField(verbose_name="Цена")
