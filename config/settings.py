79f20fab709d0efcaee310dc5da8bb935bdb8413
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-01-14 20:41:43 +0300

    deleted SSL usage for email sending

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
-EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
