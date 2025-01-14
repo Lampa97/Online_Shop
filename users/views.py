e0e32c6bf1a92f1acc315319eddbbed4c5e47133
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-01-14 20:36:27 +0300

    added sending email for registered users

--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from django.core.mail import send_mail
 from django.contrib.auth import login
 from django.views.generic.edit import FormView
 from .forms import CustomUserCreationForm
+from django.conf import settings
 
 class CustomLoginView(LoginView):
     template_name = 'login.html'
@@ -19,3 +20,16 @@ class RegisterView(FormView):
     form_class = CustomUserCreationForm
     template_name = 'register.html'
     success_url = reverse_lazy('catalog:home')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        self.send_welcome_email(user.email)
+        return super().form_valid(form)
+
+    def send_welcome_email(self, user_email):
+        subject = 'Поздравляем с регистрацией!'
+        message = "Примите наши поздравления с регистрацией в нашем сервисе! Теперь вы можете просматривать продукты и управлять ими"
+        from_email = settings.DEFAULT_FROM_EMAIL
+        recipient_list = [user_email]
+        send_mail(subject, message, from_email, recipient_list)
